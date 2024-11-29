83e717d8aacc9fc09a03e8a17b3e4457ece1ab9b
Jamil-1534 <jamil2205101534@diu.edu.bd>
2024-11-29 23:30:49 +0600

    Fast Vasrson-1

--- EMS.py
+++ EMS.py
@@ -13,8 +13,33 @@ logoLabel.grid(row=0,column=0,columnspan=2)
 leftFrame=CTkFrame(window)
 leftFrame.grid(row=1,column=0)
 
+idLabel=CTkLabel(leftFrame, text='Id', font=('arial', 18, 'bold')) 
+idLabel.grid (row=0,column=0,padx=20,pady=15,sticky='w')
+
+idEntry=CTkEntry(leftFrame, font=('arial', 15, 'bold'), width=188) 
+idEntry.grid(row=0,column=1)
+
+nameLabel=CTkLabel(leftFrame, text='Name', font=('arial', 18, 'bold')) 
+nameLabel.grid (row=1,column=0,padx=20,pady=15,sticky='w')
+
+nameEntry=CTkEntry(leftFrame, font=('arial', 15, 'bold'), width=188) 
+nameEntry.grid(row=1,column=1)
+
+phoneLabel=CTkLabel(leftFrame, text='Phone', font=('arial', 18, 'bold')) 
+phoneLabel.grid (row=2,column=0,padx=20,pady=15,sticky='w')
+
+phoneEntry=CTkEntry(leftFrame, font=('arial', 15, 'bold'), width=188) 
+phoneEntry.grid(row=2,column=1,)
+
+roleLabel=CTkLabel(leftFrame, text='role', font=('arial', 18, 'bold')) 
+roleLabel.grid (row=3,column=0,padx=20,pady=15,sticky='w')
+
+roleEntry=CTkEntry(leftFrame, font=('arial', 15, 'bold'), width=188) 
+roleEntry.grid(row=3,column=1,)
+
 rightFrame=CTkFrame(window)
 rightFrame.grid(row=1,column=1)
+
 
 window.mainloop()
 
